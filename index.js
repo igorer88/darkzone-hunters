f2e267cc8c2e13b56936dc39d193280b6896ffed
Igor Escalona <igorer88@gmail.com>
2020-09-27 04:07:40 -0400

    Remove Rest configs

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
 'use strict'
 
 const express = require('express');
-const app = express();
-const morgan = require('morgan');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-// const database = require('./db/database');
+const socket = require('socket.io');
+const database = require('./db/database');
+const events = require('./events/ws');
+
+const app = express();
 
 const host = process.env.host || 'localhost';
 const port = process.env.PORT || 5000;
-const env = process.env.NODE_ENV || 'development';
-app.locals.ENV = env;
-app.locals.ENV_DEVELOPMENT = env == 'development';
+
+app.locals.ENV= process.env.NODE_ENV || 'development';
+
+const server = app.listen(port, () => {
+  console.log(`✔ Server listening on: http://${host}:${port}`);
+});
+
+const io = socket(server);
 
 // Remove x-powered-by header (doesn't let clients know we are using Express)
 app.disable('x-powered-by');
 // Support Cross-Browsing requests
 app.use(cors());
-// Support parsing of application/json type post data
-app.use(bodyParser.json());
-// Support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
-// Returns the middleware that parses http requests log.
-app.use(morgan('dev'));
+// Static files
+app.use(express.static("public"));
 
-// error handlers
-/// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  let err = new Error('Resource Not Found');
-  err.status = 404;
-  next(err);
+io.on('connection', client => {
+  console.log('Socket > Client connected...');
+  io.emit('new user');
+
+  client.on('getFriends', async data => {
+    // console.log(data);
+    let newData = `Friends' Hunters\n`;
+    newData += await events.getFriends(data);
+    io.emit('gotFriends', newData);
+  });
+  client.on('getRandom', data => {
+    console.log(data);
+    let newData = `Random Hunters\n`;
+    io.emit('gotRandom', newData);
+  });
+
+  io.on("disconnect", () => {
+    io.emit("user disconnected");
+    console.log('Socket > Client disconnected...')
+  });
 });
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(req, res, err) {
-      res.status(err.status || 500);
-      console.error('Url: ' + req.url);
-      console.error('Message: ' + err.message);
-      console.error('Status: ' + err.status);
-      res.json({
-          message: err.message,
-          error: err,
-          title: 'error'
-      });
-  });
-}
-
-app.listen(port);
-console.log(`✔ Server listening on: http://${host}:${port}`);
 try {
-  database.db;                                       // Initialize the DB connection for testing
-  console.log(`✔ A list of hunters from MongoDB can be found at: http://${host}:${port}/api/`);
+  database.db;              // Initialize the DB connection for testing
+  // console.log(`✔ A list of hunters from MongoDB can be found at: http://${host}:${port}/api/hunters`);
 } catch (e) {
   console.error('There has been an error with the MongoDB server.');
 }
