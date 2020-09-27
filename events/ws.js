5cd300a83ddaf188af0a37bac585e2fa5f3eac67
Igor Escalona <igorer88@gmail.com>
2020-09-27 06:15:22 -0400

    Add get Random Event logic and simple validations

--- events/ws.js
+++ events/ws.js
@@ -4,51 +4,96 @@ const mongoose = require('mongoose');
 const Hunter = require('../db/models/hunter');
 
 const getFriendsEvent = async (payload) => {
-  let errorMessage = false;
+  let errorMessage = '';
   const user = JSON.parse(payload); // Parsing the payload
 
-  if (mongoose.isValidObjectId(user[0]._id.$oid)) {
+  if (mongoose.isValidObjectId(user[0]._id.$oid)) {   // Validating user's id
     const friends = user[0].friends.map(({$oid}) => $oid); // Flattening the Friends' array
 
-    if (mongoose.isValidObjectId(user[0].hunterSelected.$oid)) {
+    if (mongoose.isValidObjectId(user[0].hunterSelected.$oid)) {  // Validating hunters's id
       const _id = user[0].hunterSelected.$oid; // Getting users' selected hunter
-      const hunter = await Hunter.findById({_id}).exec(); // Getting the selected hunter
+      const hunter = await Hunter.findById({_id}); // Getting the selected hunter
 
       if (hunter) {  // If users' selected hunter is found
         const minLevel = hunter.level-10; // Setting minLevel search param
         const maxLevel = hunter.level+10; // Setting maxLevel search param
 
-        let friendsHunters = await Hunter.find({})  // Finding the hunters
+        const friendsHunters = await Hunter.find({})  // Finding the hunters
           .where('user').in(friends)
-          .where('level').gt(minLevel).lt(maxLevel)
+          .where('level').gte(minLevel).lte(maxLevel)
           .where('locked').equals(false)
           .limit(10);
 
         if (friendsHunters.length === 0) {  // If the query did not match any result return error
           errorMessage = `Friends' hunters did not match search query.`;
-          console.error(errorMessage);
+          console.error(`App > ${errorMessage}`);
           return errorMessage;
         }
-        return friendsHunters;
+        console.log(`App > Returning friends' hunters.`)
+        return friendsHunters;    // If everything's ok, return friends' hunters
+
       } else {       // If the query did not match any result return error
         errorMessage = `User's selected hunter not found.`;
-        console.error(errorMessage);
+        console.error(`App > ${errorMessage}`);
         return errorMessage;
       }
-    } else {
+    } else {        // If the hunter oid is not valid return error
       errorMessage = `Not valid hunter _id.`;
-      console.error(errorMessage);
+      console.error(`App > ${errorMessage}`);
       return errorMessage;
     }
-  } else {
+  } else {          // If the user oid is not valid return error
     errorMessage = `Not valid user _id.`;
-    console.error(errorMessage);
+    console.error(`App > ${errorMessage}`);
     return errorMessage;
   }
 };
 
 const getRandomEvent = async (payload) => {
-  console.log(payload);
+  let errorMessage = '';
+  const user = JSON.parse(payload); // Parsing the payload
+
+  if (mongoose.isValidObjectId(user[0]._id.$oid)) { // Validating user's id
+    const _id = user[0].hunterSelected.$oid; // Getting users' selected hunter
+    if (mongoose.isValidObjectId(user[0].hunterSelected.$oid)) {  // Validating hunter's id
+      const hunter = await Hunter.findById({_id}); // Getting the selected hunter
+
+      if (hunter) {
+        const minLevel = hunter.level-10; // Setting minLevel search param
+        const maxLevel = hunter.level+10; // Setting maxLevel search param
+        let random = 0;
+
+        await Hunter.countDocuments().exec(async (err, count) => {
+          random = Math.floor(Math.random() * count); // Get a random entry
+        });
+        const randomHunters = await Hunter.find()
+          .skip(random)                                 // Skiping those random entries
+          .where('level').gte(minLevel).lte(maxLevel)
+          .where('locked').equals(false)
+          .limit(10);
+        if (randomHunters.length === 0) {  // If the query did not match any result return error
+          errorMessage = `Random' hunters did not match search query.`;
+          console.error(`App > ${errorMessage}`);
+          return errorMessage;
+        }
+        console.log('App > Returning random hunters.')
+        return randomHunters;     // If everything's ok, return random hunters
+
+      } else {       // If the query did not match any result return error
+        errorMessage = `User's selected hunter not found.`;
+        console.error(`App > ${errorMessage}`);
+        return errorMessage;
+      }
+    } else {        // If the hunter's oid is not valid return error
+      errorMessage = `Not valid hunter _id.`;
+      console.error(`App > ${errorMessage}`);
+      return errorMessage;
+    }
+  } else {        // If the user's oid is not valid return error
+    errorMessage = `Not valid user _id.`;
+    console.error(`App > ${errorMessage}`);
+    return errorMessage;
+  }
 };
 
 module.exports = {
