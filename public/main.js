71d1c412265b87d3c4d334fb82df60e8d18ed2a0
Igor Escalona <igorer88@gmail.com>
2020-09-27 04:21:01 -0400

    Remove unnecesary data

--- public/main.js
+++ public/main.js
@@ -1,46 +1,38 @@
 /* eslint-disable */
+'use strict';
 
 const socket = io();
 
-let userName = "";
-let userData = "";
+let userData = '';
 
 const getUserInfo = () => {
   fetch('data/user.json')
   .then(res => res.json())
   .then(user => {
-    userData = user[0];
-    document.querySelector('.userInfo').innerHTML = `<pre>${JSON.stringify(userData)}</pre>`;
+    userData = user;
+    document.querySelector('.userInfo').innerHTML = `<pre>${JSON.stringify(user[0])}</pre>`;
   });
 };
 
-const newUserConnected = (user) => {
-  userName = user || `User${Math.floor(Math.random() * 1000000)}`;
-  socket.emit("new user", userName);
-  getUserInfo();
+const getFriends = () => {
+  socket.emit('getFriends', JSON.stringify(userData));
 };
 
 const getRandom = () => {
-  socket.emit("getRandom", userData);
+  socket.emit('getRandom', JSON.stringify(userData));
 };
 
-const getFriends = () => {
-  socket.emit('getFriends', userData);
-};
-
-// new user is created so we generate nickname and emit event
-newUserConnected();
-
-socket.on('gotRandom', (data) => {
-  const html = `<p>Get Random</p><pre>${data}</pre>`
-  document.querySelector('.messages__last').innerHTML += html;
-});
+// new user is connected so we grab the payload from the json file.
+getUserInfo();
 
 socket.on('gotFriends', (data) => {
+  console.log('gotFriends', data);
   const html = `<p>Get Friends</p><pre>${data}</pre>`
   document.querySelector('.messages__last').innerHTML += html;
 });
 
-socket.on("user disconnected", function (userName) {
-  document.querySelector(`.${userName}-userlist`).remove();
+socket.on('gotRandom', (data) => {
+  console.log('gotRandom', data);
+  const html = `<p>Get Random</p><pre>${data}</pre>`
+  document.querySelector('.messages__last').innerHTML += html;
 });
